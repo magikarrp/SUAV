b086ffe5967b6634702352e5ed30e7c19768ab94
Raymond Chu <raymondzchu@gmail.com>
2021-04-13 03:11:15 -0400

    Firebase Database
    Set up Firebase database. Put a temporary button that sends data (Hello World!) to database. Put helper classes for later use.

--- app/src/main/java/com/example/suav/MainActivity.java
+++ app/src/main/java/com/example/suav/MainActivity.java
@@ -3,13 +3,46 @@ package com.example.suav;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase rootNode;
+    private DatabaseReference reference;
+    private Button btn;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text = findViewById(R.id.text);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve a getInstance from database so we can write to it. Then we get a reference from out tree in database.
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("Pins");
+
+                // Get data and place it in the helper class
+                String hello = text.getText().toString();
+                writeDatabaseHelper writeHelper = new writeDatabaseHelper(hello);
+                // Write to database
+                reference.setValue(writeHelper);
+            }
+        });
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/suav/writeDatabaseHelper.java
@@ -0,0 +1,21 @@
+package com.example.suav;
+
+public class writeDatabaseHelper {
+
+    String text;
+
+    public writeDatabaseHelper() {
+    }
+
+    public writeDatabaseHelper(String text) {
+        this.text = text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}

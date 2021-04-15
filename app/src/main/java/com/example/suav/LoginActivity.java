c3928638d0a64cfce3dfaab634870ab7b85b645f
tsbraun1891 <tsbraun1891@gmail.com>
2021-04-15 16:34:33 -0400

    Fix login layout bug

--- app/src/main/java/com/example/suav/LoginActivity.java
+++ app/src/main/java/com/example/suav/LoginActivity.java
@@ -35,7 +35,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         // set up volley
         rq = Volley.newRequestQueue(this);
